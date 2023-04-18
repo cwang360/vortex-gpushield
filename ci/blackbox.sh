dea17b8388e0d43e93f572337344abe908b62c83
Bradley He <bhe63@gatech.edu>
2023-04-17 23:30:27 -0400

    add test program

--- ci/blackbox.sh
+++ ci/blackbox.sh
@@ -101,7 +101,6 @@ case $DRIVER in
         ;; 
     simx)
         DRIVER_PATH=$VORTEX_HOME/driver/simx
-        DEBUG_LEVEL=3
         ;;
     *)
         echo "invalid driver: $DRIVER"
@@ -115,6 +114,9 @@ then
 elif [ -d "$VORTEX_HOME/tests/regression/$APP" ];
 then
     APP_PATH=$VORTEX_HOME/tests/regression/$APP
+elif [ -d "$VORTEX_HOME/tests/runtime/$APP" ];
+then
+    APP_PATH=$VORTEX_HOME/tests/runtime/$APP
 else
     echo "Application folder found: $APP"
     exit -1
---
+++ tests/runtime/bounds_test/main.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <cassert>
+#include <vx_print.h>
+
+short d[100];
+
+int main()
+{
+	d[0] = 0;
+	for (int i = 0; i < 100; i++)
+		d[i] = d[i-1] + 2;
+	for (int i = 1; i < 100; i++)
+		assert(d[i] == d[i-1] + 2);
+
+	int a[10];
+	char b[10];
+
+	for (int i = 0; i <= 10; i++)
+		a[i] = i;
+	for (int i = 0; i < 10; i++)
+		assert(a[i] == i);
+
+	const char *c = "hello world!";
+	for (int i = 0; i < 13; i++)
+		b[i] = c[i];
+	for (int i = 0; i < 10; i++)
+		assert(b[i] == c[i]);
+
+	vx_printf("Success!\n");
+	vx_printf("Buffer base VAs: a=%p b=%p c=%p d=%p\n", a, b, c, d);
+	vx_printf("Buffer sizes: a=%x b=%x c=%x d=%x\n", 
+				10*sizeof(a[0]), 
+				10*sizeof(b[0]), 
+				13*sizeof(c[0]), 
+				100*sizeof(d[0]));
+
+	return 0;
+}
+
+// RBT layout using program output
+// The base addresses might not be deterministic, run make to double check them
+// Buffer ID (14 bits) | Base Address (48 bits) | Size (32 bits) | Read-only (1 bit) | Kernel ID (12 bit)
+// 0x1 | 0xFEFFFFC8 | 0x28 | 0x0 | 0x0
+// 0x2 | 0xFEFFFFBC | 0xA | 0x0 | 0x0
+// 0x3 | 0x8000A814 | 0xD | 0x1 | 0x0
+// 0x4 | 0x8000BA5C | 0xC8 | 0x0 | 0x0
