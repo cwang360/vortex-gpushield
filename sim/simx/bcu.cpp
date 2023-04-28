ef425d68bbf72f0f8389f54116a5b43046e4479b
cwang360 <cntawng24@gmail.com>
2023-04-27 21:02:41 -0400

    add bcu queue

--- sim/simx/bcu.cpp
+++ sim/simx/bcu.cpp
@@ -4,6 +4,24 @@
 using namespace vortex;
 
 void BcuUnit::tick() {
+    // handle rcache response
+    auto& rcache_rsp_port = core_->rcache_switch_->RspOut.at(0);
+    if (!rcache_rsp_port.empty()) {
+        auto& mem_rsp = rcache_rsp_port.front();
+        auto& entry = pending_reqs_.at(mem_rsp.tag);          
+        auto trace = entry.first;
+        DT(1, "rcache-rsp: tag=" << mem_rsp.tag << ", type=" << trace->lsu.type 
+            << ", " << *trace);  
+        assert(entry.second);
+        --entry.second; // track remaining blocks 
+        if (0 == entry.second) {
+            Output.send(trace, 1);
+            pending_reqs_.release(mem_rsp.tag);
+        } 
+        rcache_rsp_port.pop();  
+    }
+
+
     if (Input.empty())
         return;
 
@@ -18,12 +36,14 @@ void BcuUnit::tick() {
 
     DT(1, "bcu-req: " << std::hex << mem_addr.addr << ", " << *trace);
 
+    auto tag = pending_reqs_.allocate({trace, 1});
+
     // FIXME: need to properly initialize memory and request for loading RBT entry.
     MemReq mem_req;
     mem_req.addr  = mem_addr.addr;
     mem_req.write = false;
     mem_req.non_cacheable = false; 
-    mem_req.tag   = 0;
+    mem_req.tag   = tag;
     mem_req.core_id = trace->cid;
     mem_req.uuid = trace->uuid;
          
--- sim/simx/bcu.h
+++ sim/simx/bcu.h
@@ -6,15 +6,21 @@
 
 namespace vortex { 
 
+#define BCUQ_SIZE 16
+
 class Core;
 
 class BcuUnit : public SimObject<BcuUnit> {
+private:
+    HashTable<std::pair<pipeline_trace_t*, uint32_t>> pending_reqs_;
+
 public:
     SimPort<pipeline_trace_t*> Input;
     SimPort<pipeline_trace_t*> Output;
 
     BcuUnit(const SimContext& ctx, Core* core, const char* name) 
         : SimObject<BcuUnit>(ctx, name) 
+        , pending_reqs_(BCUQ_SIZE)
         , Input(this)
         , Output(this)
         , core_(core)
